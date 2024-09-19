828437de2d0d05867e734b7c7c255bed3b0f7c32
SarevalasaBharathkumar <sarevalasab@gmail.com>
2024-09-19 22:26:15 +0530

    Initial commit

---
+++ src/Components/PlacementAnalytics.js
@@ -0,0 +1,13 @@
+// src/components/JobInterview.js
+
+import React from 'react';
+
+const PlacementAnalytics = () => {
+  return (
+    <div>
+      <h2>PlacementAnalytics</h2>
+    </div>
+  );
+};
+
+export default PlacementAnalytics;
---
+++ src/Components/bellNotifications.js
@@ -0,0 +1,13 @@
+// src/components/JobInterview.js
+
+import React from 'react';
+
+const bellNotifications= () => {
+  return (
+    <div>
+      <h2>Notification Page</h2>
+    </div>
+  );
+};
+
+export default bellNotifications;
